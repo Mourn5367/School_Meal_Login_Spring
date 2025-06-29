ac414040eb375dbe604e102f9f708857ead9e50f
Mourn5367 <qwerrrt6@gmail.com>
2025-06-29 19:22:20 +0900

    Initial commit

---
+++ src/main/java/kr/ac/kopo/su/school_meal_login/SchoolMealLoginApplication.java
@@ -0,0 +1,13 @@
+package kr.ac.kopo.su.school_meal_login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolMealLoginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchoolMealLoginApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/ac/kopo/su/school_meal_login/SchoolMealLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.kopo.su.school_meal_login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolMealLoginApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
